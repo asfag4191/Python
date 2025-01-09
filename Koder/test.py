0f0fa05fec23db619308b3566f67aeabe9024f1d
Ase Fagerbakke <ase.fagerbakke@student.uib.no>
2025-01-10 00:08:17 +0100

    Initial commit

--- Koder/test.py
+++ Koder/test.py
@@ -1,17 +1,6 @@
-a = [3, 3, 2, 3, 4]
-print("Først, a =", a)  # Før listen endres
+from uib_inf100_graphics.simple import canvas, display
 
-# Mislykket forsøk på å fjerne alle 3'erne
-def should_be_removed(x):
-    return x == 3
+canvas.create_rectangle(100, 50, 300, 150, outline='red')
+canvas.create_text(200, 100, text='Hei, grafikk!', font='Arial 20 bold')
 
-for item in a:
-    print(f"Itererer over item: {item}")  # Viser hvilket element vi ser på
-    if should_be_removed(item):
-        print(f"  {item} skal fjernes (a før fjerning: {a})")
-        a.remove(item)
-        print(f"  a etter fjerning: {a}")  # Listen etter fjerning
-    else:
-        print(f"  {item} beholdes (a: {a})")  # Listen forblir uendret
-
-print("Etter, a =", a)  # Etter alle iterasjone
+display(canvas)
